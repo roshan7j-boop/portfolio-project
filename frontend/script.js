8aeb308536d6d17b36bb151e25cc7e80b443babe
roshan7j-boop <roshan7j@gmail.com>
2026-03-25 19:56:50 +0530

    Add files via upload

---
+++ frontend/script.js
@@ -0,0 +1,20 @@
+document.getElementById("contactForm").addEventListener("submit", async function(e) {
+  e.preventDefault();
+
+  const data = {
+    name: document.getElementById("name").value,
+    email: document.getElementById("email").value,
+    message: document.getElementById("message").value
+  };
+
+  const res = await fetch("http://localhost:5000/contact", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify(data)
+  });
+
+  const result = await res.json();
+  document.getElementById("response").innerText = result.message;
+});
